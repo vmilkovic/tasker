4b8c7168fefd4e75ec2b3e542a358d880ebfa124
Vedran Milković <vedran.milkovic25@gmail.com>
2021-08-24 13:13:51 +0200

    Configure JWT and refresher token services

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,8 +11,8 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Todoer</div>;
+  return <div>Tasker</div>;
 }
